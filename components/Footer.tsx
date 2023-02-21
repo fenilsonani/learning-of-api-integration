e630361a653372e25c8133275d88e2b52a65a5e9
Fenil Sonani <21bmiit160@gmail.com>
2023-02-22 00:45:08 +0530

    footer licence

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">©<Link href={process.env.githubURL} className="hover:underline">Fenil Sonani</Link>. All Rights Reserved.
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">©<Link href="https://github.com/fenilsonani/news/blob/main/LICENSE" className="hover:underline">Fenil Sonani</Link>. All Rights Reserved.
                 </span>
             </footer>
 
