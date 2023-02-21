e0e4327649e0976c343053ff99ad8eb6e859dc79
Fenil Sonani <21bmiit160@gmail.com>
2023-02-22 01:25:42 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,19 +15,10 @@ const Footer = () => {
                         <li>
                             <Link href="#" className="mr-4 hover:underline md:mr-6 ">Home</Link>
                         </li>
-                        <li>
-                            <Link href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
-                        </li>
-                        <li>
-                            <Link href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</Link>
-                        </li>
-                        <li>
-                            <Link href="#" className="hover:underline">Contact</Link>
-                        </li>
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">©<Link href="https://github.com/fenilsonani/news-api/blob/main/LICENSE" className="hover:underline">Fenil Sonani</Link>. All Rights Reserved.
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">©<Link href="https://github.com/fenilsonani/news-api/blob/deploy/LICENSE" className="hover:underline">Fenil Sonani</Link>. All Rights Reserved.
                 </span>
             </footer>
 
